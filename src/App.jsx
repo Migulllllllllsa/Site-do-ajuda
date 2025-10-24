70e08d9ef4ae9b0abfb0cf85e45fbf499106bf3e
Migulllllllllsa <147247170+Migulllllllllsa@users.noreply.github.com>
2025-10-24 14:48:12 -0300

    2ª Mudança

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 import NewsGrid from "./components/NewsGrid";
+import "./App.css";
+
+
+function ConcursosPublicos() {
+  return (
+    <div  className="TituloDosConcursos">
+      <h1>Notícias sobre concursos públicos:</h1>
+    </div>
+  );
+}
+
+function Empregos() {
+  return <h1>Empregos</h1>;
+}
+
+function Regioes() {
+  return <h1>Regiões</h1>;
+}
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <main className="container">
-        <NewsGrid />
+      <main className="content-area">
+        <Routes>
+          <Route path="/" element={<NewsGrid />} />
+          <Route path="/concursos-publicos" element={<ConcursosPublicos />} />
+          <Route path="/empregos" element={<Empregos />} />
+          <Route path="/regioes" element={<Regioes />} />
+        </Routes>
       </main>
     </div>
   );
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./Header.css";
-import logo from '../images/Image1.png';
+import logo from "../images/Image1.png";
+import { Link } from "react-router-dom";
 
-function Header() {
-   return ( 
+function header() {
+  return (
+    <header className="header">
+      <div className="logo-area">
+        <img className="Logo" src={logo} alt="Minha imagem" />
+      </div>
+      <nav className="nav">
+        <Link to="/">Início</Link>
+        <Link to="/concursos-publicos">Concursos públicos</Link>
+        <Link to="/empregos">Empregos</Link>
+        <Link to="/regioes">Regiões</Link>
+      </nav>
+    </header>
+  );
+}
 
-   <header className="header">
-     <div className="logo-area">
-       <img className="Logo" src={logo} alt="Minha imagem" />
-     </div> <nav className="nav">
-       <a href="#">Últimas notícias</a>
-       <a href="#">Concursos públicos</a>
-       <a href="#">Empregos</a> 
-       <a href="#">Regiões</a> </nav> </header> ); }
-       
-export default Header;
+export default header;
